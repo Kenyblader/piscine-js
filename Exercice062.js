f92fd2bb73b349040c4848cc966618829c331662
prince <orlandofokam@gmail.com>
2026-01-21 13:18:49 +0100

    travail prince jusqu'a exercice O92

---
+++ Exercice062.js
@@ -0,0 +1,10 @@
+function reverseArray(arr) {
+    let reversedArr = []; 
+    for (let i = arr.length - 1; i >= 0; i--) {
+        reversedArr.push(arr[i]);
+    }
+    return reversedArr;
+}
+console.log(reverseArray([1, 2, 3, 4, 5]));
+console.log(reverseArray(['a', 'b', 'c']));
+console.log(reverseArray([5]));
